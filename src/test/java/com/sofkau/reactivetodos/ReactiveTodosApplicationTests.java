1209a63df9ef1eaa56b7403e03036dba07491736
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-06-06 15:01:30 -0500

    Initial commit

---
+++ src/test/java/com/sofkau/reactivetodos/ReactiveTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.reactivetodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTodosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
